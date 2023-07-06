0de004c97714973e41fcbdc592b171d58cdddfd8
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-06 13:16:19 +0200

    feat: updated repositories to extend JPA repository

--- booknest-be/src/main/java/com/booknestapp/booknestbe/repository/BookRepository.java
+++ booknest-be/src/main/java/com/booknestapp/booknestbe/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.booknestapp.booknestbe.repository;
 
-public class BookRepository {
+import com.booknestapp.booknestbe.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
 }
+
--- booknest-be/src/main/java/com/booknestapp/booknestbe/repository/ReviewRepository.java
+++ booknest-be/src/main/java/com/booknestapp/booknestbe/repository/ReviewRepository.java
@@ -1,4 +1,11 @@
 package com.booknestapp.booknestbe.repository;
 
-public class ReviewRepository {
+import com.booknestapp.booknestbe.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
+
+
