ecb4bbaeb0cfa6af3f759d7dd99022367faef255
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-06 13:00:31 +0200

    feat: created a Book class

--- booknest-be/src/main/java/com/booknestapp/booknestbe/model/Book.java
+++ booknest-be/src/main/java/com/booknestapp/booknestbe/model/Book.java
@@ -1,4 +1,28 @@
 package com.booknestapp.booknestbe.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table
 public class Book {
+    @Id
+    private String id;
+
+    private String title;
+
+    private String author;
+
+    private String categories;
+
+    private String imageLinks;
+
+    @OneToOne(mappedBy = "book")
+    @JsonIgnore
+    private Review review;
 }
+
